97910b726d73fd937e97b1d6e61cdbfcda6c156c
abhishiek2002 <kuntalabishek2002@gmail.com>
2025-02-18 09:24:56 +0530

    initial files set up

---
+++ ReduxToolkitManageTodo/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1 className='bg-black text-white'>App</h1>
+  )
+}
+
+export default App
